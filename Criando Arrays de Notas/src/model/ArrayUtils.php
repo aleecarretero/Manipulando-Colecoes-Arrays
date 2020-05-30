a6264198cd72092449fade4bc3092ec270c366a0
Alexandre Carretero <aleecarretero@gmail.com>
2020-05-29 22:21:37 -0300

    Aula 3.3

--- Criando Arrays de Notas/src/model/ArrayUtils.php
+++ Criando Arrays de Notas/src/model/ArrayUtils.php
@@ -4,6 +4,11 @@ class ArrayUtils {
     
     public static function remover(string $elemento, array &$array){
         $posicao = array_search($elemento, $array);
-        unset($array[$posicao]);
+        if (is_int($posicao)) {
+            unset($array[$posicao]);
+            echo "{$elemento} removido com sucesso";
+        } else {
+            echo "{$elemento} não foi encontrado";
+        }
     }
 }
